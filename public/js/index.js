348424ee8be532afdbb91ea354f4ebc51ad6e46a
Tzuri Kresh <tzurik@gmail.com>
2018-10-23 12:35:14 +0200

    add message form and show messages on screen

--- public/js/index.js
+++ public/js/index.js
@@ -14,5 +14,16 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-    console.log('Got new message: ', message);    
+    console.log('Got new message: ', message);
+    let li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    $('#messages').append(li);
+});
+
+$('#message_form').on('submit', function(e) {
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'user',
+        text: $('[name=message').val()
+    }, function (data) {console.log('got it', data);});
 });
--- server/server.js
+++ server/server.js
@@ -23,9 +23,10 @@ io.on('connection', (socket) => {
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('New message created: ', message);
-        io.emit('newMessage',  generateMessage(message.from, message.text));        
+        io.emit('newMessage',  generateMessage(message.from, message.text));  
+        callback('from server');
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
